fd785e4bbac0a034105c74aa17e3c3a16b25e44a
Monge25 <emilianomongeosuna@gmail.com>
2025-12-07 20:30:38 -0700

    added problems with url and cookies

--- src/auth/PrivateRoute.tsx
+++ src/auth/PrivateRoute.tsx
@@ -1,10 +1,15 @@
 import { Navigate } from "react-router-dom";
-// import { ReactNode } from "react";
 import { useAuth } from "./AuthContext";
+import { useEffect } from "react";
 
 
 export default function PrivateRoute({ children }: any) {
   const { isAuthenticated, loading } = useAuth();
+
+  useEffect(() => {
+    // Previene que el navegador regrese a esta página desde cache
+    window.history.replaceState(null, "", window.location.href);
+  }, []);
 
   if (loading) {
     return <div className="text-white p-10">Validando sesión...</div>;
